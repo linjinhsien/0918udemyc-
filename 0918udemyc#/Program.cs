85a6936ee38f4ff3fb51f40a40ffc8f59768d805
linjinhsienmsi <iven840320@gmail.com>
2024-09-18 15:12:58 +0800

    字元字串格式1

---
+++ 0918udemyc#/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int a = 2;
+int b = 3;
+int c = a+b;
+Console.WriteLine(a);
+Console.WriteLine("c=a+b");
